c9f8052a57c4cf12d993d54d905e6d2d02470963
Scott Ballantyne <scottballantyne@protonmail.com>
2026-04-22 02:55:23 -0500

    making it easy to choose a db

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const path = require('path');
+
 if (process.env.REDIS_URL != undefined) {
   module.exports = require(path.join(__dirname, 'redis'));
-} else {
+} else if (process.env.MONGO_URL != undefined) {
   module.exports = require(path.join(__dirname, 'mongodb'));
+} else {
+  console.log("You have missing env variables");
+  console.log('Please set either the REDIS_URL or the MONGO_URL.')
+  
+  // The REDIS_URL is given priority in this file because
+  // moving load from mongodb was the point of making the redis
+  // session in the first place.
+
+  // to choose one of the versions directly just require
+  // requrie('@drifted/session/redis');
+  // or
+  // require('@drifted/session/mongodb');
 }
 
