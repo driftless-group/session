963be9f0c81654cc8d67d10dceb428581485f34f
Scott Ballantyne <scottballantyne@protonmail.com>
2026-04-22 02:49:26 -0500

    i dont think im supposed to close the connection each time I use the function

--- mongodb/client.js
+++ mongodb/client.js
@@ -1,18 +1,32 @@
 const { ObjectId, MongoClient, ServerApiVersion } = require('mongodb');
 
+var connections = {};
 
-function connect() {
+function connect(name='default') {
   return new Promise(async(resolve) => {
-    var client = new MongoClient(process.env.MONGO_URL, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
+    if (connections[name] == undefined || 
+       (connections[name] != undefined && 
+        connections[name].s.hasBeenClosed == true)) {
+      
+        // i don't know if this has been closed 
+        // stuff is the right way to detect this.
 
-    await client.connect();
-    resolve(client);
+      var client = new MongoClient(process.env.MONGO_URL, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
+
+      await client.connect();
+      connections[name] = client;
+
+      resolve(client);
+    } else {
+      
+      resolve(connections[name]);
+    }
   })
 }
 
